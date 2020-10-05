713eac4d1012afa280ad7c8a0439db17b1c71b60
Arhio <Arhio@yandex.ru>
2020-10-05 16:26:06 +0300

    Урок 8

---
+++ avitoparser/items.py
@@ -0,0 +1,20 @@
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://docs.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+from scrapy.loader.processors import MapCompose, TakeFirst
+
+def refactor_link(value):
+    if value:
+        return value.upper()
+
+
+class AvitoparserItem(scrapy.Item):
+    # define the fields for your item here like:
+    name = scrapy.Field(output_processor=TakeFirst())
+    photos = scrapy.Field(input_processor = MapCompose(refactor_link))
+    _id = scrapy.Field()
+
+
